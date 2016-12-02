96e2a4dcf517011533ae503003950fc6c54b1cd8
Taylor Tsuboi <taylort92@gmail.com>
2016-12-02 15:07:43 -0500

    added CT RNG lookup to reaction algorithm

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import re
 import logging
 import random
 import emoji
+from ctrng import CTRNG
 # commands
 import Commands
 # commands from the permission database
@@ -29,10 +30,10 @@ async def on_message(message):
     # This is an object. Name should be fetched with .name, not str(author)!
     author = message.author
     # Adds a reaction to messages at random
-    RandomNum = random.randrange(1, 100)
-    if RandomNum < 13:
+    RandomNum = CTRNG.updaterng()
+    if RandomNum['bool']:
         Reactions = [emoji.emojize(":whale:"), emoji.emojize(":dolphin:")]
-        Reaction = Reactions[RandomNum % 2]
+        Reaction = Reactions[RandomNum['num'] % 2]
         await client.add_reaction(message, Reaction)
     # This iterates over my dictionary of commands, imported from Commands.
     # The keys are the commands, and what the message needs to start with.
